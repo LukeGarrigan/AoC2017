05c5faa358721fd0c50b9f58ae8d95f3649db6a5
Luke Garrigan <lukegarrigan8@gmail.com>
2017-12-09 20:22:04 +0000

    Question 3 part 2

--- AoCDayThree/src/com/company/Main.java
+++ AoCDayThree/src/com/company/Main.java
@@ -1,15 +1,15 @@
 package com.company;
 
-import java.util.Arrays;
-
 public class Main {
 
+    static int INPUT_VALUE = 277678;
     public static void main(String[] args) {
         // write your code here
 
 
         int number = 277678;
-        System.out.println(questionOne(number));
+        // System.out.println(questionOne(number));
+        System.out.println(questionTwo(277678));
     }
 
     private static int questionOne(int number) {
@@ -18,6 +18,14 @@ public class Main {
 
         int[][] spiralArray = createSpiralArray(perfectSquare);
         return calculateManhattanDistance(spiralArray, number);
+    }
+
+    private static int questionTwo(int number) {
+        int perfectSquare = findClosestOddPerfectSquare(number);
+       // System.out.println(perfectSquare);
+
+        int value= createSpiralArrayPartTwo(perfectSquare);
+        return value;
     }
 
     private static int calculateManhattanDistance(int[][] spiralArray, int number) {
@@ -34,7 +42,7 @@ public class Main {
                 if (spiralArray[i][j] == number) {
                     xPos = i;
                     yPos = j;
-                    return Math.abs(targetX-xPos)+Math.abs(targetY-yPos);
+                    return Math.abs(targetX - xPos) + Math.abs(targetY - yPos);
                 }
             }
 
@@ -44,7 +52,162 @@ public class Main {
 
     }
 
+    private static int createSpiralArrayPartTwo(int perfectSquare) {
 
+        int dimensions = (int) Math.sqrt(perfectSquare);
+        int[][] array = new int[dimensions][dimensions];
+        int centerCordinates = (int) Math.floor(dimensions / 2);
+
+        int xPos = centerCordinates;
+        int yPos = centerCordinates;
+
+        array[xPos][yPos] = 1;
+        Direction direction = Direction.RIGHT;
+
+        int count = 0;
+        int numberDisplacement = 1;
+
+        for (int i = 1; i < perfectSquare; i++) {
+            if (direction == Direction.RIGHT) {
+                xPos = xPos + 1;
+
+                int west = 0;
+                int northWest = 0;
+                int north = 0;
+                int northEast = 0;
+                if (xPos - 1 >= 0) {
+                    west = array[xPos - 1][yPos];
+                }
+
+                if (xPos - 1 >= 0 && yPos - 1 >= 0) {
+                    northWest = array[xPos - 1][yPos - 1];
+                }
+
+                if (yPos - 1 >= 0) {
+                    north = array[xPos][yPos - 1];
+                }
+
+                if (xPos + 1 < dimensions && yPos - 1 > 0) {
+                    northEast = array[xPos + 1][yPos - 1];
+                }
+
+                array[xPos][yPos] = west + northWest + north + northEast;
+                if(array[xPos][yPos]> INPUT_VALUE){
+                    return array[xPos][yPos];
+                }
+                count++;
+                if (count >= numberDisplacement) {
+                    direction = Direction.UP;
+                    count = 0;
+                }
+            } else if (direction == Direction.UP) {
+                yPos = yPos - 1;
+
+                int south = 0;
+                int southWest = 0;
+                int west = 0;
+                int northWest = 0;
+
+                if (yPos + 1 < dimensions) {
+                    south = array[xPos][yPos + 1];
+                }
+
+                if (xPos - 1 >= 0 && yPos + 1 < dimensions) {
+                    southWest = array[xPos - 1][yPos + 1];
+                }
+
+                if (xPos - 1 >= 0) {
+                    west = array[xPos - 1][yPos];
+                }
+
+                if (xPos - 1 >= 0 && yPos - 1 >= 0) {
+                    northWest = array[xPos - 1][yPos - 1];
+                }
+
+                array[xPos][yPos] = south + southWest + west + northWest;
+                if(array[xPos][yPos]> INPUT_VALUE){
+                    return array[xPos][yPos];
+                }
+                count++;
+                if (count >= numberDisplacement) {
+                    direction = Direction.LEFT;
+                    count = 0;
+                    numberDisplacement += 1;
+                }
+            } else if (direction == Direction.LEFT) {
+                xPos = xPos - 1;
+
+                int east = 0;
+                int southEast = 0;
+                int south = 0;
+                int southWest = 0;
+
+                if (xPos + 1 < dimensions) {
+                    east = array[xPos + 1][yPos];
+                }
+
+                if (xPos + 1 < dimensions && yPos + 1 < dimensions) {
+                    southEast = array[xPos + 1][yPos + 1];
+                }
+
+                if (yPos + 1 < dimensions) {
+                    south = array[xPos][yPos + 1];
+                }
+
+                if (xPos - 1 >= 0 && yPos + 1 < dimensions) {
+                    southWest = array[xPos - 1][yPos + 1];
+                }
+                array[xPos][yPos] = east + southEast + south + southWest;
+                if(array[xPos][yPos]> INPUT_VALUE){
+                    return array[xPos][yPos];
+                }
+                count++;
+                if (count >= numberDisplacement) {
+                    direction = Direction.DOWN;
+                    count = 0;
+                }
+
+            } else if (direction == Direction.DOWN) {
+                yPos = yPos + 1;
+
+                int north = 0;
+                int northEast = 0;
+                int east = 0;
+                int southEast = 0;
+
+
+                if (yPos - 1 >= 0) {
+                    north = array[xPos][yPos - 1];
+                }
+
+                if (xPos + 1 < dimensions && yPos - 1 >= 0) {
+                    northEast = array[xPos + 1][yPos - 1];
+                }
+
+                if(xPos +1 < dimensions){
+                    east = array[xPos + 1][yPos];
+                }
+
+                if(xPos+1 < dimensions && yPos+1 <dimensions){
+                    southEast = array[xPos + 1][yPos + 1];
+                }
+
+                array[xPos][yPos] = north + northEast + east + southEast;
+                if(array[xPos][yPos]> INPUT_VALUE){
+                    return array[xPos][yPos];
+                }
+                count++;
+                if (count >= numberDisplacement) {
+                    direction = Direction.RIGHT;
+                    count = 0;
+                    numberDisplacement += 1;
+                }
+            }
+
+
+        }
+        return 0;
+    }
 
 
     private static int[][] createSpiralArray(int perfectSquare) {
@@ -62,7 +225,7 @@ public class Main {
         int count = 0;
         int numberDisplacement = 1;
 
-        for (int i = 2; i <= perfectSquare; i++) {
+        for (int i = 1; i <= perfectSquare; i++) {
             if (direction == Direction.RIGHT) {
                 xPos = xPos + 1;
                 array[xPos][yPos] = i;
@@ -116,15 +279,13 @@ public class Main {
 
     private static int findClosestOddPerfectSquare(int n) {
 
-        for(int i=1; i<Integer.MAX_VALUE;i=i+2){
-            if(i*i > n){
-                return i*i;
+        for (int i = 1; i < Integer.MAX_VALUE; i = i + 2) {
+            if (i * i > n) {
+                return i * i;
             }
         }
         return 0;
     }
-
-
 
 
 }
